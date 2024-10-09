410b48d65d70540d34aa9e8a5fb3f43a82f3cf9c
Benjamin To <quangchuongto@gmail.com>
2024-10-09 15:52:55 -0400

    prac

--- CPS305/practice.lisp
+++ CPS305/practice.lisp
@@ -1,17 +1,25 @@
+(defstruct movie
+title director year type
+  )
 (defun add-movie (movie array)
-  (let* ((title (movie-title movie))   ; Get the title of the movie
-         (found nil)                   ; Initialize a variable to track if the movie exists
-         (length (length array)))       ; Get the length of the array
-    ;; Loop through the array to check if the movie exists or find an empty slot
+  (let ((length (length array))
+        (found NIL)
+        (spot NIL)
+        )
     (dotimes (i length)
-      ;; If the slot is not empty and the title matches, set `found` to true
-      (when (and (not (null (aref array i))) 
-                 (string= (movie-title (aref array i)) title))
-        (setf found t)))
-    ;; If the movie is found or array is full, return NIL
-    (if (or found (not (find NIL array)))
+      (let ((current (aref array i)))
+        (when (and (not spot) (null (aref array i)))
+          (setf spot i)
+          )
+        (when (string= (movie-title current) (movie-title movie))
+          (setq found t)
+          )        
+        )
+      )
+    (if (or found (not spot))
         NIL
-      ;; Otherwise, find the first empty slot and add the movie
-      (setf (aref array (position NIL array)) movie)
-      array)))
+        (setf (aref array spot) movie)
+        )  
+    )
 
+  )
